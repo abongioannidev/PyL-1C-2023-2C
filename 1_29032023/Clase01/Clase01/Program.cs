4821d69fe86f7b29617972f49bcf4e4a3eeab59d
Alejandro Bongioanni <a.bongioanni@sistemas-utnfra.com.ar>
2023-03-29 22:06:19 -0300

    Clase 01

---
+++ 1_29032023/Clase01/Clase01/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+namespace Clase01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double resultadoCubo;
+            double resultadoCuadrado;
+            string valorIngresado;
+            int numero;
+
+            Console.Title = "Ejercicio 2";
+            Console.WriteLine("Ingrese un numero mayor a cero");
+
+            valorIngresado = Console.ReadLine();
+            if(int.TryParse(valorIngresado, out numero))
+            {
+                Console.WriteLine("Pude convertir");
+            }
+            else
+            {
+                Console.WriteLine("No Pude convertir");
+            }
+
+           
+            
+            while (numero < 1)
+            {
+                Console.WriteLine("ERROR. ¡Reingresar número!");
+                Console.WriteLine("Ingrese un numero mayor a cero");
+                valorIngresado = Console.ReadLine();
+
+
+                if (int.TryParse(valorIngresado, out numero))
+                {
+                    Console.WriteLine("Pude convertir");
+                }
+                else
+                {
+                    Console.WriteLine("No Pude convertir");
+                }
+            }
+
+            resultadoCuadrado = Math.Pow(numero, 2);
+            resultadoCubo = Math.Pow(numero, 3);
+
+            Console.WriteLine($"El resultado del cuadrado del numero {numero}, es {resultadoCuadrado}");
+            Console.WriteLine($"El resultado del cubo del numero {numero}, es {resultadoCubo}");
+
+            Console.ReadKey();
+
+        }
+    }
+}
