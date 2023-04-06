3f5c089240860eae6713ce39d03d2a409f2ea97d
Alejandro Bongioanni <a.bongioanni@sistemas-utnfra.com.ar>
2023-04-05 21:41:51 -0300

    delete lautaro

--- 3_05042023/Clase03/entidades/Perro.cs
+++ 3_05042023/Clase03/entidades/Perro.cs
@@ -12,12 +12,28 @@ namespace entidades
         public Persona duenio;
         public int edad;
         public string nombre;
-
+        private string raza;
+    
 
         public string Saludar()
         {
             string algo = "algo";
             return $"Soy un perrito y mi nombre es {this.nombre}";
+        }
+
+        public static void Mostrar(Perro perro)
+        {
+            Console.WriteLine(perro.Saludar());
+        }
+
+        public string GetRaza()
+        {
+            return this.raza;
+        }
+
+        public void SetRaza(string value)
+        {
+            this.raza = value;
         }
 
 
