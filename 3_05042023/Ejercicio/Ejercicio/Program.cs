a77001eb7901fc54b78f3688651c634cccc9993d
Alejandro Bongioanni <a.bongioanni@sistemas-utnfra.com.ar>
2023-04-05 23:50:37 -0300

    ejercicio boligrafo

---
+++ 3_05042023/Ejercicio/Ejercicio/Program.cs
@@ -0,0 +1,32 @@
+﻿using Entidades;
+namespace Ejercicio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string dibujo;
+            Boligrafo bic = new Boligrafo(ConsoleColor.Red,10);
+            ConsoleColor colorIncialDeConsola = Console.ForegroundColor;
+
+
+            bic.Pintar(3, out dibujo);
+            Console.ForegroundColor = bic.GetColor();
+            
+            Console.WriteLine(dibujo);
+
+            Console.ForegroundColor = colorIncialDeConsola;
+            Console.WriteLine($"cantidad de caracteres del primer retorno es {dibujo.Length}");
+
+
+            bic.Pintar(100,out dibujo);
+            Console.ForegroundColor = bic.GetColor();
+            Console.WriteLine(dibujo);
+            Console.ForegroundColor = colorIncialDeConsola;
+            Console.WriteLine($"cantidad de caracteres del segundo retorno es {dibujo.Length}");
+
+            Console.ForegroundColor = colorIncialDeConsola;
+
+        }
+    }
+}
---
+++ 3_05042023/Ejercicio/Entidades/Boligrafo.cs
@@ -0,0 +1,90 @@
+﻿using System.Text;
+
+namespace Entidades
+{
+    public class Boligrafo
+    {
+        public const short cantidadTintaMaxima = 100;
+        private ConsoleColor color;
+        private short tinta;
+
+        public Boligrafo(ConsoleColor color, short tinta)
+        {
+            this.color = color;
+            this.tinta = tinta;
+        }
+
+
+        public ConsoleColor GetColor()
+        {
+            return this.color;
+        }
+        public short GetTinta() { return this.tinta; }
+
+
+        private void SetTinta(short tinta)
+        {
+            if(tinta > 0)
+            {
+                this.Cargar(tinta);
+            }
+            else if(tinta < 0)
+            {
+                this.GastarTinta(tinta);
+            }
+        }
+
+        private void GastarTinta(short tinta)
+        {
+            int tintaAux = this.tinta - tinta;
+            if(tintaAux <= 0)
+            {
+                this.tinta = 0;
+            }
+            else
+            {
+                this.tinta = (short)tintaAux;
+
+            }
+
+        }
+
+        private void Cargar(short tinta)
+        {
+           int tintaAux =  this.tinta + tinta;
+            if(tintaAux >= Boligrafo.cantidadTintaMaxima)
+            {
+                this.tinta = Boligrafo.cantidadTintaMaxima;
+            }
+            else
+            {
+                this.tinta = (short)tintaAux;
+            }
+        }
+
+        public void Recargar()
+        {
+            this.SetTinta(Boligrafo.cantidadTintaMaxima);
+        }
+
+
+        public bool Pintar(short gasto, out string dibujo)
+        {
+            if(this.tinta <= 0) 
+            {
+                dibujo = "Sin tinta para pintar";
+            
+                return false;
+            }
+
+
+            StringBuilder sb = new StringBuilder();   
+            sb.Append('*', gasto > this.tinta ? this.tinta :gasto);
+         
+ 
+
+            dibujo = sb.ToString();
+            return true;
+        }
+    }
+}
